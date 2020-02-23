415f4899ea9c0b9d1e89021faa096cb9c4c06a40
Steven Karschnia <steven.karschnia@ndsu.edu>
2020-02-23 14:24:29 -0600

    Created generic page header

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import genericUser from './user.png'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Header() {
+    return(
+        <div className="header">
+            <h1 className="projectName">Test Project</h1>
+            <div className="user">
+                <img src={genericUser}/>
+                <p>User</p>
+            </div>
+        </div>
+    );
+}
+
+ReactDOM.render(<Header />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
