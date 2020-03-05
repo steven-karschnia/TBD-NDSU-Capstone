e0946e85c5d8bc8d82197f21e8479802fb0f63d4
Dante Norris <dante.norris@ndus.edu>
2020-03-05 11:42:11 -0600

    Free drag added, dragging from tool box adds generic element to grid

--- src/Project_Create/create.js
+++ src/Project_Create/create.js
@@ -1,11 +1,10 @@
 import React from "react";
 import RGL, { WidthProvider, Responsive } from "react-grid-layout";
 import './create.css'
+import _ from "lodash";
+
+
 const ReactGridLayout = WidthProvider(Responsive);
-
-
-
-// This is the variable storing the project layout
 const originalLayout = getFromLS("layout") || [];
 
 
@@ -18,77 +17,91 @@ export default class LocalStorageLayout extends React.PureComponent {
     className: "layout",
     cols: {lg: 12, md: 10, sm: 6, xs: 4, xxs: 2},
     rowHeight: 30,
+    verticalCompact: false,
     onLayoutChange: function() {}
   };
 
-
-
   constructor(props) {
     super(props);
+    // this.state = {
+    //   layout: JSON.parse(JSON.stringify(originalLayout))
+    // };
+
     this.state = {
-      layout: JSON.parse(JSON.stringify(originalLayout)),
-      items: [
-        {i: "1",  w: 2, h: 3, x: 0, y: 0, className: "draggable"},
-        {i: "2",  w: 2, h: 3, x: 2, y: 0, className: "draggable1" }, 
-        {i: "3",  w: 2, h: 3, x: 0, y: 0, className: "draggable2" },
-        {i: "4",  w: 2, h: 3, x: 0, y: 0, className: "draggable3" },
-        {i: "5",  w: 2, h: 3, x: 0, y: 0, className: "arrow_box" }
+      // layout: JSON.parse(JSON.stringify(originalLayout))
+      
+      layout: [1, 2, 3, 4, 5, 12].map(function(i, key, list) {
+        return {
+          i : i.toString(),
+          x : i * 2, y: 0, w: 2, h: 2,
+          add: i === (list.length - 1)
+        };
+      }),
+      toolboxElements: [
+        {i: "t0", h: 1, maxH: 1, w: 1, class: "arrow_box"},
+        {i: "t1", h: 1, w: 3, class: "draggable"}
       ],
-      templates: [
-        {i: "1", w: 3, h: 4, x: 0, y: 0}
-      ],
-      newCounter: 6
+      newCounter: 0
     };
-    this.onBreakpointChange.bind(this);
+
     this.onAddItem = this.onAddItem.bind(this);
+    this.onBreakpointChange = this.onBreakpointChange.bind(this);
     this.onLayoutChange = this.onLayoutChange.bind(this);
     this.resetLayout = this.resetLayout.bind(this);
   }
 
-  onAddItem(event) {
+  createElement(el) {
+    const removeStyle = {
+      position: "absolute",
+      right: "2px",
+      top: 0,
+      cursor: "pointer"
+    };
+    const i = el.add ? "+" : el.i
+    return (
+      <div key={i} data-grid={el}>
+        {el.add ? (
+          <span className="add text" onClick={this.onAddItem}
+                title="You can add an item by clicking here">
+            Add +
+          </span>
+
+        ) : (
+          <span className="text">{i}</span>
+        )}
+        <span className="remove" style={removeStyle}
+              onClick={this.onRemoveItem.bind(this, i)}>
+          x
+        </span>
+      </div>
+    );
+  }
+  
+  onAddItem() {
     this.setState({
-        items: this.state.items.concat({
-          i: this.state.newCounter,
-          x: (this.state.items.length * 2) % (this.state.cols || 12),
-          y: Infinity,
-          w: 2,
-          h: 2  
-        }),
-        newCounter: this.state.newCounter + 1
+      layout: this.state.layout.concat({
+        i: "n" + this.state.newCounter,
+        x: (this.state.layout.length * 2) % (this.state.cols || 12),
+        y: Infinity, w: 2, h: 2
+      }),
+      newCounter: this.state.newCounter + 1
     });
   }
 
-  // onAddItem(event) {
-  //   let tempId = event.dataTransfer.getData("tempId");
-    
-  //   this.state.templates.filter((temp) => { 
-  //     if (temp.i === tempId) {
-  //       this.setState({
-  //         items: this.state.items.concat({
-  //           i: this.state.newCounter,
-  //           x: temp.x,
-  //           y: Infinity,
-  //           w: temp.w,
-  //           h: temp.h
-  //         }),
-  //         newCounter: this.state.newCounter + 1
-  //       })
-  //     }
+
+  // onAddItem(template, elemParams) {
+  //   this.setState({
+  //     layout: this.state.layout.concat(
+  //       {id: this.state.newCounter.toString(), w: template.w, h: template.h, maxH: template.maxH, class: template.class },
+        
+  //     ),
+  //     newCounter: this.state.newCounter + 1
   //   });
   // }
-  
-
-  onBreakpointChange(breakpoint, cols) {
-    this.setState({
-      breakpoint: breakpoint,
-      cols: cols
-    })
-  }
-
 
   resetLayout() {
     this.setState({
-      layout: []
+      layout:[]
     });
   }
 
@@ -99,12 +112,77 @@ export default class LocalStorageLayout extends React.PureComponent {
     this.props.onLayoutChange(layout); // updates status display
   }
 
-  onDragStart = (event, itemID) => {
-      console.log('Beginning dragStart on div: ', itemID);        
-    	event.dataTransfer.setData("taskID", itemID);
-	}
+  onRemoveItem(i) {
+    this.setState({ items: _.reject(this.state.items, {i: i})});
+  }  
 
 
+
+  onBreakpointChange(breakpoint, cols) {
+      this.setState({
+        breakpoint: breakpoint,
+        cols: cols
+      });
+  }
+
+  onDragStart = (event, templateID) => {
+      console.log('Beginning dragStart on div: ', templateID);        
+    	event.dataTransfer.setData("templateID", templateID);
+  }
+  onDrop (event, elemParams) {
+    let templateID = event.dataTransfer.getData("templateID");
+    this.state.toolboxElements.filter((template) => {
+      if(template.i === templateID) {
+        this.onAddItem(template, elemParams);
+      }
+    });
+  }
+
+  //=============================================================
+  // render() {
+  //   return (
+  //     <div>
+        
+  //       <div>
+  //         <button onClick={(event)=>this.resetLayout}>Reset Layout</button> 
+  //         <button onClick={(event)=>this.onAddItem}>Add Element</button>
+  //         <div id="container">
+
+  //           <div id="toolbox">
+  //             <div class="draggable" key="34" data-grid={{w: 2, h: 3, x: 0, y: 0}}>
+  //               <span className="text">1</span>
+  //             </div>
+  //           </div>
+
+  //           <div id="workspace">
+  //             <ReactGridLayout
+  //               {...this.props}
+  //               layout={this.state.layout}
+  //               onLayoutChange={this.onLayoutChange}
+  //             >
+  //               <div className="draggable" key="1" data-grid={{ w: 2, h: 3, x: 0, y: 0 }}>
+  //                 <span className="text">1</span>
+  //               </div>
+  //               <div className="draggable" key="2" data-grid={{ w: 2, h: 3, x: 2, y: 0 }}>
+  //                 <span className="text">2</span>
+  //               </div>
+  //               <div className="draggable" key="3" data-grid={{ w: 2, h: 3, x: 4, y: 0 }}>
+  //                 <span className="text">3</span>
+  //               </div>
+  //               <div className="arrow_box" key="4" data-grid={{ w: 2, h: 1, x: 6, y: 0, maxH: 1 }}>
+  //                 <span className="text">4</span>
+  //               </div>
+  //               <div className="arrow_box" key="5" data-grid={{ w: 2, h: 3, x: 8, y: 0 }}>
+  //                 <span className="text">5</span>
+  //               </div>
+  //             </ReactGridLayout>
+  //           </div>
+  //          </div>
+  //        </div>
+  //      </div>
+  //   );
+  // }
+  //=====================================================
 
   render() {
     var items = {
@@ -113,7 +191,7 @@ export default class LocalStorageLayout extends React.PureComponent {
         prince2: []      
     }
 
-		this.state.items.forEach ((item) => {
+		this.state.layout.forEach ((item) => {
 		  items["agile"].push(
         <div key={item.i} data-grid={{w: item.w, h: item.h, x: item.x, y: item.y}}
               style = {{backgroundColor: "cyan"}}>
@@ -123,17 +201,16 @@ export default class LocalStorageLayout extends React.PureComponent {
     });
 
     // Pulls from the templates and creates div elements for them.
-    this.state.templates.forEach ((temp) => {
+    this.state.toolboxElements.forEach ((temp) => {
       items["template"].push(
         <div key={temp.i} data-grid={{w: temp.w, h: temp.h, x: temp.x, y: temp.y}}
-             style={{backgroundColor: "cyan"}}
+             className = {temp.class}
              onDragStart = {(event) => this.onDragStart(event, temp.i)}
              draggable >
               <span> {temp.i} </span>
           </div>
       )
     });
-    
 
   return (
       <div>
@@ -147,12 +224,10 @@ export default class LocalStorageLayout extends React.PureComponent {
           <div id="workspace">
             <ReactGridLayout
             onLayoutchange={this.onLayoutChange}
-            onDrop={(event)=>{this.onAddItem(event)}}
-            
-            // onBreakpointChange={this.onBreakpointChange}
+            isDroppable={true}
+            onDrop={(event)=>{this.onAddItem()}}
+            onBreakpointChange={this.onBreakpointChange}
             {...this.props} 
-            measureBeforeMount={false}
-            useCSSTransforms={this.state.mounted}
             >
               {items.agile}
             </ReactGridLayout>
